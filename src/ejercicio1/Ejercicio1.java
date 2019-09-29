58fb63b1808ec638c6e2b10d9f0212a10e76ffff
Jason Lopez <jason_lopez_97@hotmail.com>
2019-09-29 05:06:06 -0500

    nuevo ingreso

--- src/ejercicio1/Ejercicio1.java
+++ src/ejercicio1/Ejercicio1.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ejercicio1;
 
-/**
- *
- * @author JASON
- */
+import com.demo.view.VistaLogin;
+
 public class Ejercicio1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+     VistaLogin vl = new VistaLogin();
+     vl.setVisible(true);
     }
     
 }
